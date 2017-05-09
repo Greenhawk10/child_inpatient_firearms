f6af893dea06073332467ff1ec48f772e6706d57
Greenhawk10 <Bruceakabernie@gmail.com>
2017-05-09 10:25:38 -0400

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1yRnPbHDksCaDrqTwmeXOT48rPKMJiW98RtciBrDU0u0/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1yRnPbHDksCaDrqTwmeXOT48rPKMJiW98RtciBrDU0u0/pubhtml';
